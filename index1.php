5627c143f030e3780b24270b6e45fb397978dd3e
aninha998765 <130669141+aninha998765@users.noreply.github.com>
2023-05-26 16:00:02 -0300

    Update index1.php

--- index1.php
+++ index1.php
@@ -22,110 +22,110 @@
             case 1:
                 if($data["month"] == 1 and $data["day"] >= 20){
                     echo "<p>Aquarius</p>"; 
-                    echo "<img src='fotos/aquario.png'>";
+                    echo "<img src='aquario.png'>";
                 } else{
                     echo "<p>Capricorn</p>";
-                    echo "<img src='fotos/capricornio.png'>";
+                    echo "<img src='capricornio.png'>";
                 }
         
                 break;
             case 2:
                 if($data["month"] == 2 and $data["day"] >= 19){
                     echo "<p>Pisces</p>";
-                    echo "<img src='fotos/peixes.png'>";
+                    echo "<img src='peixes.png'>";
                 } else{
                     echo "<p>Aquarius</p>";
-                    echo "<img src='fotos/aquario.png'>";
+                    echo "<img src='aquario.png'>";
                 }
                 break;
             case 3:
                 if($data["month"] == 3 and $data["day"] >= 21){
                     echo "<p>Aries</p>";
-                    echo "<img src='fotos/aries.png'>";
+                    echo "<img src='aries.png'>";
                 } else{
                     echo "<p>Pisces</p>";
-                    echo "<img src='fotos/peixes.png'>";
+                    echo "<img src='peixes.png'>";
                 }
                 break;
             case 4:
                 if($data["month"] == 4 and $data["day"] >= 20){
                     echo "<p>Taurus</p>";
-                    echo "<img src='fotos/touro.png'>";
+                    echo "<img src='touro.png'>";
                 } else{
                     echo "<p>Aries</p>";
-                    echo "<img src='fotos/aries.png'>";
+                    echo "<img src='aries.png'>";
                 }
                 break;
             case 5:
                 if($data["month"] == 5 and $data["day"] >= 21){
                     echo "<p>Gemini</p>";
-                    echo "<img src='fotos/gemios.png'>";
+                    echo "<img src='gemini.png'>";
                 } else{
                     echo "<p>Taurus</p>";
-                    echo "<img src='fotos/touro.png'>";
+                    echo "<img src='touro.png'>";
                 }
                 break;
             case 6:
                 if($data["month"] == 6 and $data["day"] >= 21){
                     echo "<p>Cancer</p>";
-                    echo "<img src='fotos/cancer.png'>";
+                    echo "<img src='cancer.png'>";
                 } else{
                     echo "<p>Taurus</p>";
-                    echo "<img src='fotos/touro.png'>";
+                    echo "<img src='touro.png'>";
                 }
                 break;
             case 7:
                 if($data["month"] == 7 and $data["day"] >= 23){
                     echo "<p>Leo</p>";
-                    echo "<img src='fotos/leao.png'>";
+                    echo "<img src='leo.png'>";
                 } else{
                     echo "<p>Cancer</p>";
-                    echo "<img src='fotos/cancer.png'>";
+                    echo "<img src='cancer.png'>";
                 }
                 break;
             case 8:
                 if($data["month"] == 8 and $data["day"] >= 23){
                     echo "<p>Virgo</p>";
-                    echo "<img src='fotos/virgem.png'>";
+                    echo "<img src='virgem.png'>";
                 } else{
                     echo "<p>Leo</p>";
-                    echo "<img src='fotos/leao.png'>";
+                    echo "<img src='leo.png'>";
                 }
                 break;
             case 9:
                 if($data["month"] == 9 and $data["day"] >= 23){
                     echo "<p>Libra</p>";
-                    echo "<img src='fotos/libra.png'>";
+                    echo "<img src='libra.png'>";
                 } else {
                     echo "<p>Virgo</p>";
-                    echo "<img src='fotos/virgem.png'>";
+                    echo "<img src='virgem.png'>";
                 }
                 break;
             case 10:
                 if($data["month"] == 10 and $data["day"] >= 23){
                     echo "<p>Scorpio</p>";
-                    echo "<img src='fotos/escorpiao.png'>";
+                    echo "<img src='escorpiao.png'>";
                 } else {
                     echo "<p>Libra</p>";
-                    echo "<img src='fotos/libra.png'>";
+                    echo "<img src='libra.png'>";
                 }
                 break;
             case 11:
                 if($data["month"] == 11 and $data["day"] >= 22){
                     echo "<p>Sagittarius</p>";
-                    echo "<img src='fotos/sargitario.png'>";
+                    echo "<img src='sargitario.png'>";
                 } else {
                     echo "<p>Scorpio</p>";
-                    echo "<img src='fotos/escorpiao.png'>";
+                    echo "<img src='escorpiao.png'>";
                 }
                 break;
             case 12:
                 if($data["month"] == 12 and $data["day"] >= 22){
                     echo "<p>Capricorn</p>";
-                    echo "<img src='fotos/capricornio.png'>";
+                    echo "<img src='capricornio.png'>";
                 } else {
                     echo "<p>Sagittarius</p>";
-                    echo "<img src='fotos/sargitario.png'>";
+                    echo "<img src='capricornio.png'>";
                 }
                 break;
 
